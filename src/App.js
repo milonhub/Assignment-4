2ac6a075a634e621d142b5b1b02f070b2599baee
milonhub <milonru150@gmail.com>
2023-09-30 12:22:02 -0700

    m

--- src/App.js
+++ src/App.js
@@ -1,31 +1,76 @@
-import {useState} from 'react';
-import './App.css';
-import Todo from './Todo/Todo';
-import Todos from './Todo/Todos';
-import style from "./App.module.css"
+// // import UseEffect from "./UseEfect/UseEffect";
+// import { ToastContainer, toast } from 'react-toastify';
+//   import 'react-toastify/dist/ReactToastify.css';
+
+
+// function App() {
+
+//   const handleAdded= () =>{
+//        toast("Added new Todo");
+//   }
+
+//   return (
+//     <div>
+//       <h2>Todo App</h2>
+//       <button onClick={handleAdded}>Added new Todo</button>
+//       <ToastContainer />
+//     </div>
+    
+//   )
+    
+// }
+
+// export default App;
 
 
 
-function App() {
-const [userData, setUserData] = useState([])
-  const handleUser = (user) => {
-    setUserData((preuserdata) => {
-      return [...preuserdata, {user}]
-    });
-  }
-  console.log(userData)
+import React, { useEffect, useState } from 'react';
+
+import Users from './CustomHooks/Users';
+
+const App = () => {
+  // step1 : declare three states here : users, isLoading, error
+
+  const [users, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+
+// const isLoadingMessage = <p>Data is loading .....</p>
+  // step2 : use useEffect for fetching the data including updating isLoading and error states
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) =>{
+      
+      if(!res.ok) {
+        throw Error("Data is not successfuly fetched")
+      } else {
+        return res.json();
+      }
+      
+    })
+    .then((data) =>{
+      setUser(data);
+      setisLoading(false);
+
+    })
+
+    .catch((error) =>{
+      setError(error.message)
+      setisLoading(false);
+    })
+  })
+
   return (
-    <div className= {style.container}>
-      <Todos onAdduser = {handleUser}/>
+    <div className="container">
+      <h1 className="title">Users Management App</h1>
+      {isLoading && <p>Loading users...</p>}
+      {error && <p>{error}</p>}
+      {/* step3 : pass the users data to Users component  */}
 
-      <div className={style.dflex}>
-      {userData.map((user)=> {
-        return <Todo user = {user} key= {Math.random()}/>
-      })}
-      </div>
-     
+      <Users users= {users}/>
     </div>
   );
-}
+};
 
 export default App;
